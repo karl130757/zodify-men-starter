cffc8ee9b0c8878887b4b65ef16298db35be8d98
karl130757 <karl130757@gmail.com>
2025-02-09 01:58:16 +0800

    Feat:Helment-Implementation

--- backend/src/configs/app.ts
+++ backend/src/configs/app.ts
@@ -2,11 +2,15 @@ import express from 'express';
 import routes from '../routes';
 import corsMiddleware from '../middlewares/corsMiddleware';
 import { errorHandlerMiddleware } from '../middlewares/errorMiddleware';
+import helmetConfig from '../configs/helmetConfig';
 
 const app = express();
 
 app.use(corsMiddleware);
+app.use(helmetConfig(true));
+
 app.use(express.json());
+
 app.use('/api', routes);
 app.use(errorHandlerMiddleware);
 
